4041f4adcb85f163e04af1edcdc36395e8777ff5
André Silva <andpsilva@gmail.com>
2022-03-29 23:56:45 +0100

    minor changes

--- ToDo.py
+++ ToDo.py
@@ -76,90 +76,27 @@ class ToDo():
 		
 		self.tasks.sort(key= lambda x: x.completed)
 	
-	def getTasks(self):
-		return self.tasks
-	
-	def getTask_byid(self, id):
-		l = [x for x in self.tasks if x.id == id]
-		if len(l) > 1:
-			print("Error, more than 1 task with same id")
-			exit()
-		return l[0]
-		
-		# Same as this
-		# for t in self.tasks:
-		# 	if t.id == id:
-		# 		return t
-	
-	def printTasks(self):
-		for task in self.tasks:
-			task.printTask()
-			print("------")
-
 	def addTask(self, title, description, deadline):
 		newTask = task(title, description, False, deadline, self.generator.getInt())
 
 		#When adding new task add at the beggining, its not completed
 		self.tasks.insert(0, newTask)
-
-	def setCompleted(self, id):
-		for task in self.tasks:
-			if task.id == id:
-				task.completed = True
-				break
-		
-		#sort so it goes to end
-		self.tasks.sort(key= lambda x: x.completed)
-
-	#edits the parameters that are passed, the others stay the same
-	def editTask(self, id, new_title=None, new_description=None, new_deadline=None, new_completed=None):
-		idx_task = -1
-		for idx, task in enumerate(self.tasks):
-			if task.id == id:
-				idx_task = idx
-				break
-		
-		if idx_task == -1:
-			return
-		
-		#Edit the task in index idx_task
-		if new_title != None:
-			self.tasks[idx_task].title = new_title
-		
-		if new_description != None:
-			self.tasks[idx_task].description = new_description
-		
-		if new_deadline != None:
-			self.tasks[idx_task].deadline = new_deadline
-		
-		if new_completed != None:
-			self.tasks[idx_task].completed = new_completed
 	
-	def setIncompleted(self, id):
+	def printTasks(self):
 		for task in self.tasks:
-			if task.id == id:
-				task.completed = False
-				break
-		
-		#sort so it goes to beginning
-		self.tasks.sort(key= lambda x: x.completed)
-
-	def delTask(self, id):
-		idx_delete = -1
-		for idx, task in enumerate(self.tasks):
-			if task.id == id:
-				idx_delete = idx
-				break
-		
-		if idx_delete == -1:
-			return
-		
-		self.tasks.pop(idx_delete)
+			task.printTask()
+			print("------")
+	
+	def save(self):
+		with open("savefile", "wb") as f:
+			pickle.dump(self.tasks, f)
 
 	def load(self):
-		with open("savefile", "rb") as f:
-			self.tasks = pickle.load(f)
-		
+		try:
+			with open("savefile", "rb") as f:
+				self.tasks = pickle.load(f)
+		except:
+			return
 		#Add the ids of the generated tasks to the generator so it doesnt generates this again
 		added = [task.id for task in self.tasks]
 		# Same as this
@@ -169,9 +106,59 @@ class ToDo():
 		
 		self.generator.addGenerated(added)
 	
-	def save(self):
-		with open("savefile", "wb") as f:
-			pickle.dump(self.tasks, f)
+	def setCompleted(self, id):
+		for task in self.tasks:
+			if task.id == id:
+				task.completed = True
+				break
+		
+		#sort so it goes to end
+		self.tasks.sort(key= lambda x: x.completed)
+
+	def setIncompleted(self, id):
+		for task in self.tasks:
+			if task.id == id:
+				task.completed = False
+				break
+		
+		#sort so it goes to beginning
+		self.tasks.sort(key= lambda x: x.completed)
+	
+	def delTask(self, id):
+		for task in self.tasks:
+			if task.id == id:
+				self.tasks.pop( self.tasks.index(task) )
+				return
+		
+	#edits the parameters that are passed, the others stay the same
+	def editTask(self, id, new_title=None, new_description=None, new_deadline=None):
+		for task in self.tasks:
+			if task.id == id:			
+				#Edit the task in index idx_task
+				if new_title != None:
+					task.title = new_title
+				
+				if new_description != None:
+					task.description = new_description
+				
+				if new_deadline != None:
+					task.deadline = new_deadline
+			
+			return
+
+	def getTasks(self):
+		return self.tasks
+
+	def getTask_byid(self, id):
+		l = [x for x in self.tasks if x.id == id]
+		if len(l) != 1:
+			print("Error, more than 1 task with same id")
+			exit()
+		return l[0]
+		# Same as this
+		# for t in self.tasks:
+		# 	if t.id == id:
+		# 		return t
 
 
 if __name__ == "__main__":
@@ -202,7 +189,7 @@ if __name__ == "__main__":
 						hour=int(hour), minute=int(minute))
 
 				app.addTask(title, description, deadline)
-				print("Adicionado!!")
+				print("Added!!")
 			except:
 				print("Error, try again")
 		elif a == 'print':
@@ -213,6 +200,27 @@ if __name__ == "__main__":
 		elif a == 'incomplete':
 			id = int(input("ID of task to incomplete: "))
 			app.setIncompleted(id)
+		elif a == 'edit':
+			id = int(input("ID of task to edit: "))
+			new_title = input("Título: ")
+			new_description = input("Descrição: ")
+
+			new_deadline = None
+			if input("Do you want to add a deadline? (Y/N) ").lower() == "y":
+				deadline = None
+				if input("Do you want to add a deadline? (Y/N) ").lower() == "y":
+					data = input("Insert date in format DD/MM/AAAA HH:MM : ")
+
+					data = data.split(" ")
+
+					day, month, year  = data[0].split("/")
+					hour, minute = data[1].split(":")
+
+					new_deadline = datetime.datetime(int(year), int(month), int(day), 
+						hour=int(hour), minute=int(minute))
+
+			app.editTask(id, new_title, new_description, new_deadline)
+			print("Updated!!")
 		elif a == 'delete':
 			id = int(input("ID of task to delete: "))
 			app.delTask(id)
